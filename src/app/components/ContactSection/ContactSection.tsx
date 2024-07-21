e96cf6c039ac763a669b85efa5bc60c775ada0e8
Ujjawal Joshi <ujjawal.joshi@gyizer.com>
2024-07-22 00:53:36 +0530

    CHORE:addition of formik

--- src/app/components/ContactSection/ContactSection.tsx
+++ src/app/components/ContactSection/ContactSection.tsx
@@ -1,8 +1,48 @@
+"use client";
 import React from "react";
 import "./contact-section.css";
+import { Formik } from "formik";
+import { initialValues } from "./contact-section-data";
+import { contactFormOnSubmit } from "./contact-form-function";
 
 const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <>
+      <Formik initialValues={initialValues} onSubmit={contactFormOnSubmit}>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="message"
+              name="message"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.message}
+            />
+            {errors.message && touched.message && errors.message}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default ContactSection;
--- src/app/components/FooterSection/FooterSection.tsx
+++ src/app/components/FooterSection/FooterSection.tsx
@@ -3,9 +3,11 @@ import { hostedDomain } from "./footer-data";
 import "./footer.css";
 
 const FooterSection = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer__wrapper">
-      Copyright &#169; {hostedDomain} | All rights reserved
+      Copyright &#169; {currentYear} {hostedDomain} | All rights reserved
     </div>
   );
 };
